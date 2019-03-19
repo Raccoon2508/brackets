e43c1f99621f4caa302401e585d7f63046e3d2f2
Raccoon2508 <45191288+Raccoon2508@users.noreply.github.com>
2019-03-19 17:05:41 +0300

    feat: passing brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  
+  let openedPar=[];
+  let closedPar=[];
+
+  let stackOpened=[];
+  
+
+
+  for(let i=0; i<bracketsConfig.length; i++){
+    let arrConf=bracketsConfig[i]
+    openedPar.push(arrConf[0]);
+    closedPar.push(arrConf[1]);
+    
+  }
+
+  let strToArray=str.split('');
+
+  for(let i=0; i<strToArray.length; i++){
+    if(openedPar.indexOf(strToArray[i])>=0&&closedPar.indexOf(strToArray[i])<0){
+      stackOpened.push(openedPar[openedPar.indexOf(strToArray[i])]);
+    }
+
+    if(openedPar.indexOf(strToArray[i])>=0&&closedPar.indexOf(strToArray[i])>=0){
+      if(stackOpened.lastIndexOf((strToArray[i]),[i-1])<0){
+        stackOpened.push(strToArray[i]);
+      }else{
+         stackOpened.pop();
+      }
+    }
+
+    if(openedPar.indexOf(strToArray[i])<0&&closedPar.indexOf(strToArray[i])>=0){
+      if(stackOpened[stackOpened.lenght-1]==closedPar[openedPar.indexOf(strToArray[i])]){
+        stackOpened.pop();
+      }
+
+
+    }
+
+  }
+
+ if(stackOpened==0){ 
+   return true;}
+   else{
+     return false;
+   }
+ } 
